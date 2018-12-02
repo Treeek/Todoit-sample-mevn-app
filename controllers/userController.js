016a35a49e5a9f0709db75787665043cc55a8e75
Treeek <johngc2010@hotmail.com>
2018-12-02 14:47:20 -0200

    create saving test for userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,13 @@
+const User = require("../models/user");
+
 let userController = {};
+
+userController.save = async function (req, res) {
+	const user = new User(req.body);
+	user.save().then(() => {
+		res.statusMessage = "Success";
+		res.status(200).send();
+	});
+};
 
 module.exports = userController;
--- test/controllers_test/userController_test.js
+++ test/controllers_test/userController_test.js
@@ -1,7 +1,32 @@
+const assert = require("assert");
+const User = require("../../models/user");
 const userController = require("../../controllers/userController");
+const http_mocks = require("node-mocks-http");
+
+function buildResponse() {
+	return http_mocks.createResponse({ eventEmitter: require("events").EventEmitter });
+}
 
 describe("User Controller", () => {
 	it("Saves a user into the database", (done) => {
-		
+		const res = buildResponse();
+		const req = http_mocks.createRequest({
+			method: "POST",
+			url: "/users",
+		});
+
+		req.body = {
+			email: "jojotimao@hotmail.com",
+			password: "1234556",
+			name: "John"
+		};
+
+		res.on("end", () => {
+			User.findOne({ name: "John" }).then((response) => {
+				assert(response.name === "John" && res.statusCode === 200 && res.statusMessage === "Success");
+				done();
+			});
+		});
+		userController.save(req, res);
 	});
 });
