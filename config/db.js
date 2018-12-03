52cf7b9b41e6d8ac6a0dbee9e36fc98ba8296955
Treeek <johngc2010@hotmail.com>
2018-12-03 11:55:05 -0200

    integration dabase to server

--- config/db.js
+++ config/db.js
@@ -2,8 +2,4 @@ var mongoose = require("mongoose");
 mongoose.connect("mongodb://john:123456A@ds044577.mlab.com:44577/todoit");
 var db = mongoose.connection;
 
-db.once("open", function () {
-
-	console.log("DB connected!");
-
-}).on("error", console.error.bind(console, "connection error:"));
+module.exports = db;
--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
 
+const db = require("./config/db");
+
 const routerApi = require("./routes/routes");
-app.use(require("body-parser".json()));
+
+
+app.use(require("body-parser").urlencoded({
+	extended: true
+}));
 app.use("/api", routerApi);
 app.get("/", (req, res) => {
 	res.send("Hello Guys");
 });
 
-app.listen(5500);
+db.once("open", function () {
+	console.log("DB connected!");
+	app.listen(5500);
+}).on("error", console.error.bind(console, "connection error:"));
+
